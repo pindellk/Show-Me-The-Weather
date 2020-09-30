8ee40681ea0ca539cbc572343ee564df921710d4
pindellk <pindellk@protonmail.com>
2020-09-29 20:52:30 -0400

    Add 5 day forecast data pull/display

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,18 @@
+// TO DO
+// 1. Set default data
+// 2. One city search at a time - don't add
+// 3. Get icons
+// 4. Streamline so that forecast data is looped
+// 5. Link city searches
+// 6. Add Fahrenheit function
+
 // Current: https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=0b58d2e4fda20e7f139806a12aba40d1
 // Forecast: https://api.openweathermap.org/data/2.5/forecast?q=atlanta&appid=0b58d2e4fda20e7f139806a12aba40d1
 
 // Set default data
 
 // Current day variables
-var cityDate = $("#city-date"); 
+var cityDate = $("#city-date");
 var currentDay = moment().format('L');
 var icon = $("#weather-icon"); // NEED TO GRAB ICON
 var temp = $("#temperature");
@@ -34,7 +42,7 @@ $("#find-city").on("click", function (event) {
     var queryFutureURL = "https://api.openweathermap.org/data/2.5/forecast?q=" +
         city + APIKey;
 
-    // Get and display city search
+    // Get and display city search  - CLEAR PREVIOUS RESULTS?
     cityDate.prepend(city + " ");
     $("#current-card").append(+cityDate);
 
@@ -49,7 +57,7 @@ $("#find-city").on("click", function (event) {
         method: "GET"
     })
         .then(function (response) {
-            icon.text(response.weather[0].icon);  // FIGURE OUT
+            icon.text(response.weather.icon);  // FIGURE OUT
             var celTemp = response.main.temp;
             var Fahrenheit = ((celTemp - 273.15) * 1.80 + 32).toFixed(1);
             temp.text("Temperature: " + Fahrenheit + " °F");
@@ -64,19 +72,57 @@ $("#find-city").on("click", function (event) {
         method: "GET"
     })
         .then(function (response) {
-           
-            // For loop for 5 days?
 
-            // Get first forecast date - can streamline
+            // For loop for 5 days? - can streamline
+            // Get first forecast date
+
+            // Day 1
             var future1 = moment().add(1, 'day').format('L');
             day1.text(future1);
-            
-            // var celTemp = response.main.temp;
-            // var Fahrenheit = ((celTemp - 273.15) * 1.80 + 32).toFixed(1);
 
+            var celTemp = response.list[0].main.temp;
+            var Fahrenheit = ((celTemp - 273.15) * 1.80 + 32).toFixed(1);
+            // $("future-icon-1").text(response.list[0].weather.icon);;  // FIGURE OUT
+            $("#future-temp-1").text("Temperature: " + Fahrenheit + " °F");
+            $("#future-hum-1").text("Humidity: " + response.list[0].main.humidity + "%");
+
+            // Day 2
+            var future2 = moment().add(2, 'day').format('L');
+            day2.text(future2);
+
+            var celTemp = response.list[8].main.temp;
+            var Fahrenheit = ((celTemp - 273.15) * 1.80 + 32).toFixed(1);
             // icon.text(response.weather[0].icon);;  // FIGURE OUT
-            // temp.text("Temperature: " + Fahrenheit + " °F");
-            // humidity.text("Humidity: " + response.main.humidity + "%");
-           
+            $("#future-temp-2").text("Temperature: " + Fahrenheit + " °F");
+            $("#future-hum-2").text("Humidity: " + response.list[8].main.humidity + "%");
+
+            // Day 3
+            var future3 = moment().add(3, 'day').format('L');
+            day3.text(future3);
+
+            var celTemp = response.list[16].main.temp;
+            var Fahrenheit = ((celTemp - 273.15) * 1.80 + 32).toFixed(1);
+            // icon.text(response.weather[0].icon);;  // FIGURE OUT
+            $("#future-temp-3").text("Temperature: " + Fahrenheit + " °F");
+            $("#future-hum-3").text("Humidity: " + response.list[16].main.humidity + "%");
+
+            var future4 = moment().add(4, 'day').format('L');
+            day4.text(future4);
+
+            var celTemp = response.list[24].main.temp;
+            var Fahrenheit = ((celTemp - 273.15) * 1.80 + 32).toFixed(1);
+            // icon.text(response.weather[0].icon);;  // FIGURE OUT
+            $("#future-temp-4").text("Temperature: " + Fahrenheit + " °F");
+            $("#future-hum-4").text("Humidity: " + response.list[24].main.humidity + "%");
+
+            var future5 = moment().add(5, 'day').format('L');
+            day5.text(future5);
+
+            var celTemp = response.list[32].main.temp;
+            var Fahrenheit = ((celTemp - 273.15) * 1.80 + 32).toFixed(1);
+            // icon.text(response.weather[0].icon);;  // FIGURE OUT
+            $("#future-temp-5").text("Temperature: " + Fahrenheit + " °F");
+            $("#future-hum-5").text("Humidity: " + response.list[32].main.humidity + "%");
+
         });
 });
