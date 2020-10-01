ff687059a95a7150ccaa652a42bfd617779c96d0
pindellk <pindellk@protonmail.com>
2020-10-01 00:14:03 -0400

    Add icons, update element IDs

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,3 @@
-// TO DO
-// Get icons
-// Streamline so that forecast data is looped
-
 // Current day variables
 var cityDate = $("#date");
 var currentDay = moment().format('l');
@@ -39,57 +35,55 @@ function getCityWeather(city) {
     $.ajax({
         url: queryURL,
         method: "GET"
-    })
-        .then(function (response) {
-            // icon.attr("src", "https://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png");
-            temp.text("Temperature: " + response.main.temp.toFixed(1) + " °F");
-            humidity.text("Humidity: " + response.main.humidity + "%");
-            wind.text("Wind Speed: " + response.wind.speed + " MPH");
-            description.text("Description: " + response.weather[0].description);
-        });
+    }).then(function (response) {
+        icon.attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+        temp.text("Temperature: " + response.main.temp.toFixed(1) + " °F");
+        humidity.text("Humidity: " + response.main.humidity + "%");
+        wind.text("Wind Speed: " + response.wind.speed + " MPH");
+        description.text("Description: " + response.weather[0].description);
+    });
 
     // Get forecast weather
     $.ajax({
         url: queryFutureURL,
         method: "GET"
-    })
-        .then(function (response) {
+    }).then(function (response) {
 
-            // Day 1
-            var future1 = moment().add(1, 'day').format('l');
-            day1.text(future1);
-            // $("future-icon-1").text(response.list[0].weather.icon);;  // FIGURE OUT
-            $("#future-temp-1").text("Temp: " + response.list[0].main.temp.toFixed(1) + " °F");
-            $("#future-hum-1").text("Humidity: " + response.list[0].main.humidity + "%");
+        // Day 1
+        var future1 = moment().add(1, 'day').format('l');
+        day1.text(future1);
+        $("#future-icon-1").attr("src", "https://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+        $("#future-temp-1").text("Temp: " + response.list[0].main.temp.toFixed(1) + " °F");
+        $("#future-hum-1").text("Humidity: " + response.list[0].main.humidity + "%");
 
-            // Day 2
-            var future2 = moment().add(2, 'day').format('l');
-            day2.text(future2);
-            // icon.text(response.weather[0].icon);;  // FIGURE OUT
-            $("#future-temp-2").text("Temp: " + response.list[8].main.temp.toFixed(1) + " °F");
-            $("#future-hum-2").text("Humidity: " + response.list[8].main.humidity + "%");
+        // Day 2
+        var future2 = moment().add(2, 'day').format('l');
+        day2.text(future2);
+        $("#future-icon-2").attr("src", "https://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png");
+        $("#future-temp-2").text("Temp: " + response.list[8].main.temp.toFixed(1) + " °F");
+        $("#future-hum-2").text("Humidity: " + response.list[8].main.humidity + "%");
 
-            // Day 3
-            var future3 = moment().add(3, 'day').format('l');
-            day3.text(future3);
-            // icon.text(response.weather[0].icon);;  // FIGURE OUT
-            $("#future-temp-3").text("Temp: " + response.list[16].main.temp.toFixed(1) + " °F");
-            $("#future-hum-3").text("Humidity: " + response.list[16].main.humidity + "%");
+        // Day 3
+        var future3 = moment().add(3, 'day').format('l');
+        day3.text(future3);
+        $("#future-icon-3").attr("src", "https://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png");
+        $("#future-temp-3").text("Temp: " + response.list[16].main.temp.toFixed(1) + " °F");
+        $("#future-hum-3").text("Humidity: " + response.list[16].main.humidity + "%");
 
-            // Day 4
-            var future4 = moment().add(4, 'day').format('l');
-            day4.text(future4);
-            // icon.text(response.weather[0].icon);;  // FIGURE OUT
-            $("#future-temp-4").text("Temp: " + response.list[24].main.temp.toFixed(1) + " °F");
-            $("#future-hum-4").text("Humidity: " + response.list[24].main.humidity + "%");
+        // Day 4
+        var future4 = moment().add(4, 'day').format('l');
+        day4.text(future4);
+        $("#future-icon-4").attr("src", "https://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png");
+        $("#future-temp-4").text("Temp: " + response.list[24].main.temp.toFixed(1) + " °F");
+        $("#future-hum-4").text("Humidity: " + response.list[24].main.humidity + "%");
 
-            // Day 5
-            var future5 = moment().add(5, 'day').format('l');
-            day5.text(future5);
-            // icon.text(response.weather[0].icon);;  // FIGURE OUT
-            $("#future-temp-5").text("Temp: " + response.list[32].main.temp.toFixed(1) + " °F");
-            $("#future-hum-5").text("Humidity: " + response.list[32].main.humidity + "%");
-        });
+        // Day 5
+        var future5 = moment().add(5, 'day').format('l');
+        day5.text(future5);
+        $("#future-icon-5").attr("src", "https://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png");
+        $("#future-temp-5").text("Temp: " + response.list[32].main.temp.toFixed(1) + " °F");
+        $("#future-hum-5").text("Humidity: " + response.list[32].main.humidity + "%");
+    });
 }
 
 // Search button on-click event
